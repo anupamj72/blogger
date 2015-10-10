4c3b19f1d585c5e58c87da78ee05a97b62cc36c8
anupamj72 <anupamj72@gmail.com>
2015-10-10 15:29:26 +0530

    create new post feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,13 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
+  def create
+	@article = Article.new
+	@article.title = params[:article][:title]
+	@article.body = params[:article][:body]
+	@article.save
+	redirect_to articles_path()
+  end
   
   
 end
